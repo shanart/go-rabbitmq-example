63f213c8bde160a9e0b9a096d0c06c0e4aec96ec
shanart <shanin.x@gmail.com>
2023-02-24 10:27:11 +0200

    basic Sender

--- sender/main.go
+++ sender/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"context"
 	"log"
-	"os"
-	"strings"
+	"math"
 	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -16,50 +15,89 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+type RSender struct {
+	ch     *amqp.Channel
+	exName string
+}
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	err = ch.ExchangeDeclare(
-		"logs",   // name
-		"fanout", // type
-		true,     // durable
-		false,    // auto-deleted
-		false,    // internal
-		false,    // no-wait
-		nil,      // arguments
-	)
-	failOnError(err, "Failed to declare an exchange")
-
+func (rs *RSender) Push(message string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	body := bodyFrom(os.Args)
-	err = ch.PublishWithContext(ctx,
-		"logs", // exchange
-		"",     // routing key
-		false,  // mandatory
-		false,  // immediate
+	body := message
+	err := rs.ch.PublishWithContext(ctx,
+		rs.exName, // exchange
+		"",        // routing key
+		false,     // mandatory
+		false,     // immediate
 		amqp.Publishing{
 			ContentType: "text/plain",
 			Body:        []byte(body),
-		})
+		},
+	)
 	failOnError(err, "Failed to publish a message")
-
 	log.Printf(" [x] Sent %s", body)
+	return nil
 }
 
-func bodyFrom(args []string) string {
-	var s string
-	if (len(args) < 2) || os.Args[1] == "" {
-		s = "hello"
-	} else {
-		s = strings.Join(args[1:], " ")
+func CreateRSender(conn *amqp.Connection, exchangeName string) *RSender {
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	err = ch.ExchangeDeclare(
+		exchangeName, // name
+		"fanout",     // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+	return &RSender{
+		ch:     ch,
+		exName: exchangeName,
 	}
-	return s
+}
+
+func main() {
+	conn, err := connect("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	rsender := CreateRSender(conn, "logs")
+	defer rsender.ch.Close()
+
+	rsender.Push("Hello rabbitmq")
+	failOnError(err, "Failed to send message")
+}
+
+func connect(connectionString string) (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	for {
+		c, err := amqp.Dial(connectionString)
+		if err != nil {
+			log.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			log.Println("Connected to RabbitMQ")
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			log.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
 }
